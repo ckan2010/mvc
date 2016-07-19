80e36bd0c299dda8ac387e20ef794fde0b944df4
ckan <ckan@hb2008>
2016-07-19 17:49:05 +0900

    2016-07-19 mvc 2

--- mvc/WebContent/WEB-INF/global/home.jsp
+++ mvc/WebContent/WEB-INF/global/home.jsp
@@ -1,12 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%-- <jsp:include page="${context}/global/top.jsp" />
-<jsp:include page="${context}/global/header.jsp" /> --%>
+<jsp:include page="${context}/top.do" />
+<jsp:include page="${context}/header.do" /> 
 <div class="box">
 	 <h2>서비스를 이용하시려면 회원가입을 하셔야 합니다.</h2>
 	 <%-- <a href="${context}/member/service/regist.jsp">회원가입 하러 가기</a><br />
 	<a href="${context}/member/service/login.jsp">로그인 하러 가기</a><br />
 	<a href="${context}/global/main.jsp">메인으로 가기</a> --%>
 </div>
-       홈에 도착함
-<%-- <jsp:include page="${context}/global/footer.jsp"/>
-<jsp:include page="${context}/global/end.jsp"/> --%>
+       회원가입 홈
+<jsp:include page="${context}/footer.do"/>
+<jsp:include page="${context}/end.do"/>
--- mvc/src/home/HomeController.java
+++ mvc/src/home/HomeController.java
@@ -9,26 +9,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import javafx.scene.shape.Path;
+import jdk.nashorn.internal.ir.RuntimeNode.Request;
+
 /**
  * Servlet implementation class HomeController
  */
-@WebServlet("/home.do")
+@WebServlet({"/home.do","/top.do","/header.do","/footer.do","/end.do"})
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HomeController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dis = request.getRequestDispatcher("/WEB-INF/global/home.jsp");
+		String servletPath = request.getServletPath();
+		String path = servletPath.split("/")[1];
+		String view = path.substring(0, path.indexOf("."));
+		System.out.println("가야할경로 : "+view);
+		RequestDispatcher dis = request.getRequestDispatcher("/WEB-INF/global/"+view+".jsp");
 		dis.forward(request, response);
 	}
 
